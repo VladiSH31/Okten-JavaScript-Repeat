cba7d92de4bb6410b8574119a46ac4b8b3eb17b8
VladiSH31 <vladislav.sheremet@ukr.net>
2025-03-04 11:49:25 +0200

    -made cw 1 lesson 3

---
+++ Lesson 3/CW/cw3.js
@@ -0,0 +1,25 @@
+// JS preview | Інкремент / декременти
+
+let x =0;
+x = x + 1;
+console.log(x);
+
+x++;
+x++;
+x++;
+x++;
+console.log(x);
+
+x-=1;
+console.log(x);
+
+x-=2;
+console.log(x);
+
+x*=2;
+console.log(x);
+
+// JS preview |  Цикл for of
+// JS preview |  Цикл for
+// JS preview | Цикл for in
+// JS preview |  Цикл while
