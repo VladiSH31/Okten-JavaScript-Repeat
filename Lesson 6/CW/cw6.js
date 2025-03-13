d6f62f47589fafa1f960181a90971ef65c7128f0
VladiSH31 <vladislav.sheremet@ukr.net>
2025-03-13 13:47:52 +0200

    -made cw lesson 6

--- Lesson 6/CW/cw6.js
+++ Lesson 6/CW/cw6.js
@@ -1,72 +1,65 @@
-// let users = [
-//     {name: 'vasya', age: 31, status: false},
-//     {name: 'petya', age: 30, status: true},
-//     {name: 'kolya', age: 29, status: true},
-//     {name: 'olya', age: 28, status: false},
-//     {name: 'max', age: 30, status: true},
-//     {name: 'anya', age: 31, status: false},
-//     {name: 'oleg', age: 28, status: false},
-//     {name: 'andrey', age: 29, status: true},
-//     {name: 'masha', age: 30, status: true},
-//     {name: 'olya', age: 31, status: false},
-//     {name: 'max', age: 31, status: true}
-// ];
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
 
-// users.forEach(function (value, index) {
-//     console.log(value, index)
+// users.forEach(value => console.log(value))
+//
+// let filtredUsers = users.filter(function (value) {
+//     return value.age>30
 // });
+//
+// console.log(filtredUsers);
 
-// users.forEach(value => console.log(value));
-
-// users.filter(value => {
-//     if (value.age > 30) {
-//         console.log(value)
-//     }
-// });
-
-// let mappedUsers = users.map((value, index) => {
-//     return {
+// let mapedUsers = users.map((value, index) => {
+//     let newUser = {
 //         name: value.name,
 //         age: value.age,
 //         status: value.status,
 //         id: index + 1
-//     };
+//     }
+//     return newUser;
+// });
+//
+// console.log(mapedUsers);
+
+// let sortedAge = users.sort((a, b) => {
+//     return b.age - a.age
 // })
 //
-// console.log(mappedUsers);
-
-// let findedName = users.find(value => value.name === 'olya');
-// console.log(findedName);
-
-// console.log(users.every(value => value.status));
-// console.log(users.some(value => value.status));
-
-// let sorted = users.sort((a, b) => {
-//     return a.age - b.age;
-// })
+// console.log(sortedAge);
 //
-// console.log(sorted);
-
-// let sortedUsers = users.sort((a, b) => {
-//     if (a.name > b.name){
+// let sortedName = users.sort((a, b) => {
+//     if (a.name > b.name) {
 //         return 1;
 //     }
-//     if (a.name < b.name){
+//     if (a.name < b.name) {
 //         return -1;
 //     }
-//     if (a.name === b.name){
+//     if (a.name === b.name) {
 //         return 0;
 //     }
 // })
-// console.log(sortedUsers);
+//
+// console.log(sortedName);
 
 let reducedUsers = users.reduce((previousValue, currentValue) => {
-    if (currentValue.status) {
-        previousValue.statusT.push(currentValue)
-    } else {
-        previousValue.statusF.push(currentValue)
+    if (currentValue.age >= 30) {
+        previousValue[0].push(currentValue)
+    }
+    if (currentValue.age < 30) {
+        previousValue[1].push(currentValue)
     }
     return previousValue;
-}, {statusT:[], statusF:[]})
+}, [[], []])
 
-console.log(reducedUsers);
+console.log(reducedUsers[0]);
